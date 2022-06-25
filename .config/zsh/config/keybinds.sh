104bfa85e3839a8a405e45e965ac81be9a0ce1d9
Dellon Zerus <dellonzerus@gmail.com>
2022-06-25 12:42:08 -0700

    Adjusted word movement keybindings

--- .config/zsh/config/keybinds.sh
+++ .config/zsh/config/keybinds.sh
@@ -10,12 +10,12 @@ zle -N edit-command-line
 bindkey '^x' edit-command-line
 
 # Ctrl + o/n moves forward/backward by one word
-bindkey '^o' forward-word
-bindkey '^n' backward-word
+bindkey '^[o' forward-word
+bindkey '^[n' backward-word
 
 # Alt + o/n moves to beginning/end of line
-bindkey '^[n' beginning-of-line
-bindkey '^[o' end-of-line
+bindkey '^n' beginning-of-line
+bindkey '^o' end-of-line
 
 # Use Alt + e/i to move through history
 bindkey '^[e' down-history
