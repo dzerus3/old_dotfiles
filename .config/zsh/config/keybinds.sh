f229690accf6d607b19a8c13b75acd192715fc6e
Dellon Zerus <dellonzerus@gmail.com>
2022-06-26 15:19:55 -0700

    Added neio single character movement for zsh

--- .config/zsh/config/keybinds.sh
+++ .config/zsh/config/keybinds.sh
@@ -9,13 +9,17 @@ autoload edit-command-line
 zle -N edit-command-line
 bindkey '^x' edit-command-line
 
-# Ctrl + o/n moves forward/backward by one word
+# Ctrl + o/n moves one character forward/back
+bindkey '^o' forward-char
+bindkey '^n' backward-char
+
+# Alt + o/n moves forward/backward by one word
 bindkey '^[o' forward-word
 bindkey '^[n' backward-word
 
-# Alt + o/n moves to beginning/end of line
-bindkey '^n' beginning-of-line
-bindkey '^o' end-of-line
+# Ctrl + e/i moves to beginning/end of line
+bindkey '^e' beginning-of-line
+bindkey '^i' end-of-line
 
 # Use Alt + e/i to move through history
 bindkey '^[e' down-history
