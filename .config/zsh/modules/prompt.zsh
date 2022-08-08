cbe9566fc77f9c4bd8ea5e8bde116cee476daba8
Dellon Zerus <dellonzerus@gmail.com>
2022-08-08 12:13:37 -0700

    Fixed prompt when plugins are not installed

--- .config/zsh/modules/prompt.zsh
+++ .config/zsh/modules/prompt.zsh
@@ -1,19 +1,23 @@
-# Don't throw an error if git-prompt plugin is not installed
-# TODO: I don't think it actually works correctly
-if typeset -f gitprompt > /dev/null; then
-	function gitprompt{}
-fi
-
 # Prompt variable reference in
 ZSH_THEME_GIT_PROMPT_PREFIX=':'
 ZSH_THEME_GIT_PROMPT_SUFFIX=
 ZSH_THEME_GIT_PROMPT_BRANCH="%{$fg_bold[white]%}"
 ZSH_THEME_GIT_PROMPT_SEPARATOR=' '
 
-# Sets a red and magenta prompt if root, and blue/green
-# otherwise.
-if [ $UID -eq 0 ]; then
-	PROMPT='%B%{$fg[red]%}[%{$fg[magenta]%}%B%1~%b%{$reset_color%}$(gitprompt)%{$fg[red]%}]%{$reset_color%} '
+# Sets a red and magenta prompt if root, and blue/green otherwise.
+if typeset -f gitprompt > /dev/null; then
+	if [ $UID -eq 0 ]; then
+		PROMPT='%B%{$fg[red]%}[%{$fg[magenta]%}%B%1~%b%{$reset_color%}$(gitprompt)%{$fg[red]%}]%{$reset_color%} '
+	else
+		PROMPT='%B%{$fg[blue]%}[%{$fg[green]%}%B%1~%b%{$reset_color%}$(gitprompt)%{$fg[blue]%}]%{$reset_color%} '
+	fi
 else
-	PROMPT='%B%{$fg[blue]%}[%{$fg[green]%}%B%1~%b%{$reset_color%}$(gitprompt)%{$fg[blue]%}]%{$reset_color%} '
+if [ $UID -eq 0 ]
+	then
+		PS1="%B%{$fg[red]%}[%{$fg[magenta]%}%B%1~%b%{$reset_color%}%{$fg[red]%}]%{$reset_color%} "
+	else
+		PS1="%B%{$fg[blue]%}[%{$fg[green]%}%B%1~%b%{$reset_color%}%{$fg[blue]%}]%{$reset_color%} "
+	fi
 fi
+
+
