8658098d1c8ca003edb3a73ab2468805831c00af
Dellon Zerus <dellonzerus@gmail.com>
2023-01-19 11:03:07 -0800

    zsh alias tweaks

--- .config/zsh/modules/custom.zsh
+++ .config/zsh/modules/custom.zsh
@@ -1,26 +1,28 @@
 # A few common abbreviations
 abbrev cls='clear'
 abbrev md='mkdir'
-abbrev q='exit'
 
 abbrev nv='nvim'
 
 abbrev e='exa'
 abbrev el='exa -l'
 abbrev ea='exa -a'
+abbrev eal='exa -al'
+abbrev ela='exa -la'
+abbrev eld='exa -ld'
+abbrev edl='exa -dl'
 
-abbrev hw='task +hw'
-abbrev tadd='task add'
-abbrev tann='task annotate'
-abbrev tmod='task modify'
-abbrev tall='task all'
-abbrev tsta='task start'
-abbrev tlist='task list'
-abbrev tdone='task done'
-abbrev tmodlast='task modify $(task +LATEST uuids)'
-abbrev tdep='task add dep:$(task +LATEST uuids)'
-
-abbrev ti='timew'
+if command -v task &> /dev/null; then
+    abbrev tadd='task add'
+    abbrev tann='task annotate'
+    abbrev tmod='task modify'
+    abbrev tall='task all'
+    abbrev tsta='task start'
+    abbrev tlist='task list'
+    abbrev tdone='task done'
+    abbrev tmodlast='task modify $(task +LATEST uuids)'
+    abbrev tdep='task add dep:$(task +LATEST uuids)'
+fi
 
 # Enables colors for diff if GNU coreutils are installed
 ls   --version | grep GNU > /dev/null && alias ls='ls --color=auto'
@@ -57,17 +59,6 @@ alias yt-music='yt-dlp --extract-audio --audio-format opus --yes-playlist -o "%(
 alias yt-audiobook='yt-dlp --extract-audio --audio-format mp3 --yes-playlist -o "%(title)s.%(ext)s"'
 
 alias wget="wget --hsts-file $XDG_CACHE_HOME/wget-hsts"
-
-
-# TODO See also
-# https://www.freecodecamp.org/news/tmux-in-practice-integration-with-system-clipboard-bcd72c62ff7b/
-# https://unix.stackexchange.com/questions/211817/copy-the-contents-of-a-file-into-the-clipboard-without-displaying-its-contents/211826#211826
-# https://github.com/zshzoo/copier/blob/main/copier.zsh
-if command -v wl-copy &> /dev/null; then
-	abbrev copy='wl-copy'
-elif command -v xcopy &> /dev/null; then
-	abbrev copy='xcopy'
-fi
 
 open(){
     nohup xdg-open $1 > /dev/null &
