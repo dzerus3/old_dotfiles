acb1eda72b8b43b4ce66b9f08eb02a50f71e50bd
Dellon Zerus <dellonzerus@gmail.com>
2023-02-11 18:35:57 -0800

    Improve ZSH prompt

--- .config/zsh/modules/prompt.zsh
+++ .config/zsh/modules/prompt.zsh
@@ -4,20 +4,29 @@ ZSH_THEME_GIT_PROMPT_SUFFIX=
 ZSH_THEME_GIT_PROMPT_BRANCH="%{$fg_bold[white]%}"
 ZSH_THEME_GIT_PROMPT_SEPARATOR=' '
 
-# Sets a red and magenta prompt if root, and blue/green otherwise.
-if typeset -f gitprompt > /dev/null; then
-    if [ $UID -eq 0 ]; then
-        PROMPT='%B%{$fg[red]%}[%{$fg[magenta]%}%B%1~%b%{$reset_color%}$(gitprompt)%{$fg[red]%}]%{$reset_color%} '
-    else
-        PROMPT='%B%{$fg[blue]%}[%{$fg[green]%}%B%1~%b%{$reset_color%}$(gitprompt)%{$fg[blue]%}]%{$reset_color%} '
-    fi
-else
-if [ $UID -eq 0 ]
-    then
-        PS1="%B%{$fg[red]%}[%{$fg[magenta]%}%B%1~%b%{$reset_color%}%{$fg[red]%}]%{$reset_color%} "
-    else
-        PS1="%B%{$fg[blue]%}[%{$fg[green]%}%B%1~%b%{$reset_color%}%{$fg[blue]%}]%{$reset_color%} "
-    fi
+# Resets prompt
+PROMPT=""
+
+# If we're connected to ssh, display hostname
+if ((${#SSH_CLIENT[@]})); then
+    PROMPT=$PROMPT'($(hostname)) '
 fi
 
+# Sets a red and magenta prompt if root, and blue/green otherwise.
+if [ $UID -eq 0 ]; then
+    PROMPT=$PROMPT"%B%{$fg[red]%}[%{$fg[magenta]%}%B%1~%b%{$reset_color%}"
+else
+    PROMPT=$PROMPT"%B%{$fg[blue]%}[%{$fg[green]%}%B%1~%b%{$reset_color%}"
+fi
 
+# If gitprompt addon is installed, use it
+if typeset -f gitprompt > /dev/null; then
+    PROMPT=$PROMPT'$(gitprompt)'
+fi
+
+# Sets a red and magenta prompt if root, and blue/green otherwise.
+if [ $UID -eq 0 ]; then
+    PROMPT=$PROMPT"%{$fg[red]%}]%{$reset_color%} "
+else
+    PROMPT=$PROMPT"%{$fg[blue]%}]%{$reset_color%} "
+fi
