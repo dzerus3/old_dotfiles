4bbd38b4c6f72c3a877f266f380596933eea34c9
Dellon Zerus <dellonzerus@gmail.com>
2023-03-01 16:59:19 -0800

    Remove plugins from install script

--- .local/bin/dotfiles_install.sh
+++ .local/bin/dotfiles_install.sh
@@ -21,31 +21,10 @@ git --git-dir=$HOME/.local/share/dotfiles --work-tree=$HOME\
     remote add origin https://github.com/dzerus3/dotfiles
 # Pull in the dotfiles
 git --git-dir=$HOME/.local/share/dotfiles --work-tree=$HOME\
-    pull origin master
+    pull origin minimal
 # Set Github as upstream
 git --git-dir=$HOME/.local/share/dotfiles --work-tree=$HOME\
-    branch --set-upstream-to=origin/master master
-
-echo "Enabling plugins..."
-
-# Creates zsh plugins folder (zsh will auto install them from there)
-mkdir -p $HOME/.config/zsh/plugins
-
-# Installs Packer (neovim package manager)
-git clone --depth 1 https://github.com/wbthomason/packer.nvim\
-    $HOME/.local/share/nvim/site/pack/packer/start/packer.nvim
-
-# Disables neovim plugins temporarily
-sed -i '1i\
-    vim.g.disableplugins = 1
-' $XDG_CONFIG_HOME/nvim/init.lua
-
-# Runs PackerSync to get all other plugins installed
-nvim -c "PackerSync" $(mktemp)
-
-# Re-enable neovim plugins
-sed '1d' $XDG_CONFIG_HOME/nvim/init.lua > $XDG_CONFIG_HOME/nvim/tmp.lua
-mv $XDG_CONFIG_HOME/nvim/tmp.lua $XDG_CONFIG_HOME/nvim/init.lua
+    branch --set-upstream-to=origin/minimal minimal
 
 echo "Neovim may have given you a treesitter-related error. This is normal."
 echo "Installation seems to have finished successfully. Yay."
