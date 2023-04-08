f0d5c87a8c97e30cad4854aafeb605f902df9205
Dellon Zerus <dellonzerus@gmail.com>
2023-04-08 14:07:57 -0700

    Moved nvim statusbar outside plugins section

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -402,59 +402,6 @@ then
     ]])
 
     ---------------------------------------------------------------
-    -- Status line
-    --
-    -- Inspired by elements of:
-    -- https://stackoverflow.com/a/5380230
-    -- https://stackoverflow.com/a/10416234
-    -- https://github.com/amix/vimrc
-    ---------------------------------------------------------------
-
-    -- Always show the status line
-    vim.opt.laststatus=2
-    -- Enables 24-bit colors
-    vim.opt.termguicolors=true
-
-    -- Colors for status line
-    vim.cmd([[
-        highlight User1 guifg=#CE933B guibg=#333333
-        highlight User2 guifg=#76CE3B guibg=#333333
-        highlight User3 guifg=#933BCE guibg=#333333
-        highlight User4 guifg=#EA5031 guibg=#333333
-        highlight User5 guifg=#F2E863 guibg=#333333
-        highlight User6 guifg=#CCCCCC guibg=#333333
-        highlight User7 guifg=#FFFFFF guibg=#333333
-    ]])
-
-    vim.opt.statusline = table.concat({
-        -- Buffer number
-        "%1* %02n %*",
-        -- File name (up to 40 characters)
-        "%2*%<%.40t%*",
-        -- File type
-        "%3* %Y %*",
-        -- Flags (modified, readonly, help, etc.)
-        "%4*%m%r%w%h%*",
-        -- Separator
-        "%1*%=",
-        -- Column number
-        "%5*%c%6*:",
-        -- Row number/Number of rows
-        "%5*%l%6*/%1*%L",
-        -- Percentage of location
-        "%7*%6p%%  "
-    })
-
-    -- If inside a plaintext file, add word count to status line
-    vim.cmd([[
-        augroup statusline
-            autocmd!
-            autocmd FileType markdown,text,plaintex
-            \ set statusline+=\ w:%{WordCount()}\ \ 
-        augroup end
-    ]])
-
-    ---------------------------------------------------------------
     -- Telescope
     ---------------------------------------------------------------
 
@@ -586,6 +533,59 @@ then
 
     vim.keymap.set("n", "<leader>u", vim.cmd.UndotreeToggle)
 end
+
+---------------------------------------------------------------
+-- Status line
+--
+-- Inspired by elements of:
+-- https://stackoverflow.com/a/5380230
+-- https://stackoverflow.com/a/10416234
+-- https://github.com/amix/vimrc
+---------------------------------------------------------------
+
+-- Always show the status line
+vim.opt.laststatus=2
+-- Enables 24-bit colors
+vim.opt.termguicolors=true
+
+-- Colors for status line
+vim.cmd([[
+    highlight User1 guifg=#CE933B guibg=#333333
+    highlight User2 guifg=#76CE3B guibg=#333333
+    highlight User3 guifg=#933BCE guibg=#333333
+    highlight User4 guifg=#EA5031 guibg=#333333
+    highlight User5 guifg=#F2E863 guibg=#333333
+    highlight User6 guifg=#CCCCCC guibg=#333333
+    highlight User7 guifg=#FFFFFF guibg=#333333
+]])
+
+vim.opt.statusline = table.concat({
+    -- Buffer number
+    "%1* %02n %*",
+    -- File name (up to 40 characters)
+    "%2*%<%.40t%*",
+    -- File type
+    "%3* %Y %*",
+    -- Flags (modified, readonly, help, etc.)
+    "%4*%m%r%w%h%*",
+    -- Separator
+    "%1*%=",
+    -- Column number
+    "%5*%c%6*:",
+    -- Row number/Number of rows
+    "%5*%l%6*/%1*%L",
+    -- Percentage of location
+    "%7*%6p%%  "
+})
+
+-- If inside a plaintext file, add word count to status line
+vim.cmd([[
+    augroup statusline
+    autocmd!
+    autocmd FileType markdown,text,plaintex
+        \ set statusline+=\ w:%{WordCount()}\ \ 
+    augroup end
+]])
 
 ---------------------------------------------------------------
 -- Packer
